65cf24ba5b6261e2c65659c48aa7a625923de249
aaronc1028 <81634761+aaronc1028@users.noreply.github.com>
2021-06-20 20:12:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function update(){
+    document.getElementById ("img1").src="WIN_20210620_19_54_21_Pro.jpg"
+  }
+  function update(){
+    document.getElementById ("img1").src="WIN_20210620_19_55_15_Pro.jpg"
+      }
