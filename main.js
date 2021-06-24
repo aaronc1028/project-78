cdca87aacfdfd34db33f388b67a9c13fa2f5cdd3
aaronc1028 <81634761+aaronc1028@users.noreply.github.com>
2021-06-23 21:23:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+var Family=["WIN_20210620_19_54_21_Pro.jpg","WIN_20210620_19_55_15_Pro.jpg","20210618_081438.jpg","WIN_20210623_21_03_11_Pro.jpg"]
+var i=0
+
 function update(){
-    document.getElementById ("img1").src="WIN_20210620_19_54_21_Pro.jpg"
+    document.getElementById ("img1").src=Family[i]
+    i++
+
   }
-  function update(){
-    document.getElementById ("img1").src="WIN_20210620_19_55_15_Pro.jpg"
-      }
+  
